74e059cbf3696a54f7387e6941c6197dcb422781
AlmironLeandro <leandroealmiron@hotmail.com>
2023-01-22 16:49:15 -0300

    Fin sección 10 - version 3.0.0

--- routes/user.route.js
+++ routes/user.route.js
@@ -7,8 +7,11 @@ const {
     usuarioDelete,
     usuarioPatch } = require('../controllers/user.controller');
 const { esRoleValido, emailExiste, existeUsuarioPorId } = require('../helpers/db-validators');
-const { validarCampos } = require('../middlewares/validar-campos');
-const { validarJWT } = require('../middlewares/validar-jwt');
+// const { validarCampos } = require('../middlewares/validar-campos');
+// const { validarJWT } = require('../middlewares/validar-jwt');
+// const { esAdminRole, tieneRole } = require('../middlewares/validar-roles');
+
+const { validarCampos, validarJWT, esAdminRole, tieneRole } = require('../middlewares');
 
 const router = Router();
 
@@ -40,6 +43,8 @@ router.post('/',
 
 router.delete('/:id', [
     validarJWT,
+    // esAdminRole,
+    tieneRole('ADMIN_ROLE', 'VENTAS_ROLE'),
     check('id', 'No es un ID válido').isMongoId(),
     check('id').custom(existeUsuarioPorId),
     validarCampos
